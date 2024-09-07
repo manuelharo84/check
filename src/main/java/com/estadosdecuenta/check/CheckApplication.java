647bb2930782f52050d4cdf133da1b27835cf9d3
Manuel Haro <manuelharo84@gmail.com>
2024-09-06 23:42:48 -0500

    Corrección de errores

--- src/main/java/com/estadosdecuenta/check/CheckApplication.java
+++ src/main/java/com/estadosdecuenta/check/CheckApplication.java
@@ -2,7 +2,6 @@ package com.estadosdecuenta.check;
 
 import java.io.IOException;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
